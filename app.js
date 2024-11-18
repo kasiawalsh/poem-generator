ee2353796acd9dc9d654cbc4b1f2b84df0b29f1e
kasiawalsh <kasia_walsh@yahoo.co.uk>
2024-11-18 21:48:59 +0000

    Add OpenAi conenction - needs work

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import chatCompletion from './services/openai.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from my lovely new server!')
-});
+app.get(
+    '/',
+    (req, res) => {
+        res.send('Hello from my lovely new server!');
+    }
+);
 
-export default app;
+app.get(
+    '/test',
+    (req, res) => {
+        res.send('Hello from my lovely new server /test!');
+    }
+    // async (req, res) => {
+    //     const response = await chatCompletion("Provide an amusing sentence.");
+    //     res.send(response);
+    // }
+);
+
+export default app; 
